841de3cf0e044c785132bd5b53a18f65c16db4a3
80003509 <80003509@sfmail.sf-express.com>
2018-10-25 09:35:13 +0800

    正则

---
+++ test/src/test/com/zhengzebiaodashi/Client.java
@@ -0,0 +1,16 @@
+package test.com.zhengzebiaodashi;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class Client {
+	public static void main(String[] args) {
+		
+		Pattern p = Pattern.compile("^[0-9]{12}|^[A-Za-z]{2}[0-9]{13}$");
+		System.err.println(p.matcher("SF1236352152596").matches());
+		System.err.println("dd56".matches("^[A-Za-z]{2}[0-9]{1}$"));
+		String s = "070036987522,856230644535,856230647555256asasas";
+		String[] sw= s.split("\n", s.length());
+		System.err.println(Arrays.toString(sw));
+	}
+}
